6738f1642006a14586ef096db33b60f826a266d4
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2023-12-19 22:45:50 +0100

    JDBC Example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,18 +10,22 @@ public class Main {
         //Establishing a connection to the MySQL database using the DriverManager class:
         try(final Connection connection = DriverManager.getConnection(System.getenv("DB_URL"), System.getenv("DB_USER"),System.getenv("DB_PASSWORD"))){
 
+            if (connection.isValid(0)) {
+                System.out.println("Connection successful");
+            }
+
             Statement statement = connection.createStatement();
 
             final ResultSet resultSet = statement.executeQuery("SELECT * FROM employee");
             while (resultSet.next()) {
-                System.out.println("employee id: " + resultSet.getInt("employee_id"));
-                System.out.println("first name: " + resultSet.getString("first_name"));
-                System.out.println("last name: " + resultSet.getString("last_name"));
-                System.out.println("email: " + resultSet.getString("email"));
-                System.out.println("phone number: " + resultSet.getInt("phone_number"));
-                System.out.println("hire date: " + resultSet.getDate("hire_date"));
-                System.out.println("job title: " + resultSet.getString("job_title"));
-                System.out.println("salary: " + resultSet.getDouble("salary"));
+                System.out.println("Employee id: " + resultSet.getInt("employee_id"));
+                System.out.println("First name: " + resultSet.getString("first_name"));
+                System.out.println("Last name: " + resultSet.getString("last_name"));
+                System.out.println("Email: " + resultSet.getString("email"));
+                System.out.println("Phone number: " + resultSet.getInt("phone_number"));
+                System.out.println("Hire date: " + resultSet.getDate("hire_date"));
+                System.out.println("Job title: " + resultSet.getString("job_title"));
+                System.out.println("Salary: " + resultSet.getDouble("salary"));
                 System.out.println("-----------------------------------------");
             }
         } catch (SQLException exp) {
@@ -37,7 +41,7 @@ public class Main {
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement statement = connection.prepareStatement("SELECT * FROM employee WHERE first_name = ? OR employee_id = ?");
 
-            int rowCount = statement.executeUpdate("INSERT INTO  employee VALUES (3, 'Jack', 'jack', 'jack@gmail.com',2345,'2002-01-02','Java Programmer',700)");
+            int rowCount = statement.executeUpdate("INSERT INTO  employee VALUES (4, 'Daniel', 'daniel', 'daniel@gmail.com',2345,'2002-01-02','Java Programmer',700)");
             statement.setString(1, "Leo");
             statement.setInt(2, 2);
 
@@ -49,6 +53,23 @@ public class Main {
 
         } catch (SQLException exp) {
             exp.printStackTrace();
+        }
+
+
+        //Drop table
+        drop();
+    }
+
+    private static void drop() {
+        String connectionUrl = System.getenv("DB_URL");
+        String username = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl, username, password);
+             Statement dropStatement = connection.createStatement()) {
+            dropStatement.execute("DROP TABLE IF EXISTS departments");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
